7b6223ce78e5dfb56460734949942a23a273a716
Tom Wood <woody2371@gmail.com>
2020-08-31 09:41:49 +0800

    Added last modified. Final commit for initial modules.

--- dashboard.py
+++ dashboard.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 from jinja2 import Environment, PackageLoader, select_autoescape
 from datetime import datetime
 import fbdataprocessing.processcsv as fbdata
+import os.path, time
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     fbdata.loadDicts()
-    return render_template('template.html', pickDict=fbdata.filterproductDict('pickitemstatusId',['10','11'],True), commitDict=fbdata.committedDict(), backorderDict=fbdata.filterproductDict('pickitemstatusId',['5'],True), totalqty=0, date=datetime.today(), sodict=fbdata.fullSoDict())
+    lastUpdated = time.ctime(os.path.getmtime('C:\Dashboard\Dashboard Website\static\dbexport\export.csv'))
+    return render_template('template.html', pickDict=fbdata.filterproductDict('pickitemstatusId',['10','11'],True), commitDict=fbdata.committedDict(), backorderDict=fbdata.filterproductDict('pickitemstatusId',['5'],True), totalqty=0, date=datetime.today(), sodict=fbdata.fullSoDict(), lastUpdated=lastUpdated)
